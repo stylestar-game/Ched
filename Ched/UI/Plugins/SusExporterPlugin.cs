a9e82852ee18f1fdabe565fd37613e447cb566c7
Paralleltree <paralleltree@outlook.com>
2018-09-08 17:33:00 +0900

    SusExporterPlugin追加

---
+++ Ched/UI/Plugins/SusExporterPlugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+
+using Ched.Components;
+using Ched.Components.Exporter;
+
+namespace Ched.UI.Plugins.Exporter
+{
+    public class SusExporterPlugin : IExportablePlugin
+    {
+        private SusExporter exporter = new SusExporter();
+
+        public string Filter => "Seaurchin Score File(*.sus)|*.sus";
+        public string DisplayName => "susフォーマット";
+        public IExporter Exporter => exporter;
+
+        public Form GetForm(ScoreBook book)
+        {
+            return new SusExportForm(book, exporter);
+        }
+
+        public string GetCustomData()
+        {
+            return JsonConvert.SerializeObject(exporter.CustomArgs);
+        }
+
+        public void SetCustomData(string data)
+        {
+            exporter.CustomArgs = JsonConvert.DeserializeObject<SusArgs>(data);
+        }
+    }
+}
